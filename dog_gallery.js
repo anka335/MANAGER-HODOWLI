a1dcf4579e2fa6ccce863f1d081ad59f060e9648
anka335 <joanna.zubik@uczen.zsk.poznan.pl>
2023-09-23 22:29:57 +0200

    wersja 1.2 (nasi podopieczni)

---
+++ dog_gallery.js
@@ -0,0 +1,169 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let buttons = document.getElementsByTagName("button");
+    var buttonArray = Array.from(buttons);
+    let arrows = document.getElementsByClassName("arrow");
+    var arrowsArray = Array.from(arrows);
+    arrowsArray.forEach(function(element){
+            element.classList.add("less");
+    });
+    let leftArrows = document.getElementsByClassName("arrow_left");
+    var leftArrowsArray = Array.from(leftArrows);
+    leftArrowsArray.forEach(function(element){
+            element.classList.add("less");
+    });
+    let rightArrows = document.getElementsByClassName("arrow_right");
+    var rightArrowsArray = Array.from(rightArrows);
+    rightArrowsArray.forEach(function(element){
+            element.classList.add("less");
+    });
+    let numberOfImgs = 4;
+    if (window.screen.width > 1200){
+        numberOfImgs = 4;
+    } else if (window.screen.width > 1000) {
+        numberOfImgs = 3;
+    } else if (window.screen.width > 850){
+        numberOfImgs = 2;
+    } else {
+        numberOfImgs = 1;
+    }
+    
+    window.addEventListener("resize", (event) => {
+        console.log("ZMIANA WIELKOSC");
+        if (window.screen.width > 1200){
+            numberOfImgs = 4;
+        } else if (window.screen.width > 1000) {
+            numberOfImgs = 3;
+        } else if (window.screen.width > 850){
+            numberOfImgs = 2;
+        } else {
+            numberOfImgs = 1;
+        }
+    });
+
+    buttonArray.forEach(element => {
+        element.setAttribute("is_one", "1");
+        let gallery = element.parentElement;
+        let images = gallery.getElementsByClassName("dog_image");
+        var imagesArray = Array.from(images).slice(1);
+        imagesArray.forEach(function(element) {
+            element.classList.add("less");
+        });
+        let thisArrows = gallery.getElementsByClassName("arrow");
+        var thisArrowsArray = Array.from(thisArrows);
+        thisArrowsArray.forEach(function(element){
+            element.classList.add("less");
+        });
+
+        element.addEventListener("click", function() {
+            console.log("numberOfImgs: ", numberOfImgs);
+            gallery = element.parentElement;
+            images = gallery.getElementsByClassName("dog_image");
+            imagesArray = Array.from(images).slice(1);
+            imagesArray.forEach(function(element) {
+                element.classList.add("less");
+            });
+            
+            let attribute = element.getAttribute("is_one");
+            if (attribute === "1"){
+                attribute = "0";
+                element.textContent = "mniej";
+                for (let i = 0; i < Math.min(imagesArray.length, numberOfImgs-1); i++) {
+                    imagesArray[i].classList.add("more");
+                    imagesArray[i].classList.remove("less");
+                }
+                if (imagesArray.length > numberOfImgs-1){
+                    for (let i = numberOfImgs-1; i < imagesArray.length; i++) {
+                        imagesArray[i].classList.add("less");
+                        imagesArray[i].classList.remove("more");
+                    }
+                }
+                thisArrowsArray.forEach(function(arrow) {
+                    arrow.classList.replace("less", "more");
+                });
+
+            } else {
+                attribute = "1";
+                element.textContent = "więcej...";
+                for (let i = 0; i < imagesArray.length; i++) {
+                    imagesArray[i].classList.add("less");
+                    imagesArray[i].classList.remove("more");
+                }
+                thisArrowsArray.forEach(function(arrow) {
+                    arrow.classList.replace("more", "less");
+                });
+            }
+            element.setAttribute("is_one", attribute);
+            console.log(attribute);
+        });
+    });
+
+
+    leftArrowsArray.forEach(element => {
+        element.addEventListener("click", function(){
+            let gallery = element.parentElement;
+            let thisArrows = gallery.getElementsByClassName("arrow");
+            let thisArrowsArray = Array.from(thisArrows);
+            let images = gallery.getElementsByClassName("dog_image");
+            var imagesArray = Array.from(images);
+            gallery.innerHTML = '';
+
+            gallery.appendChild(thisArrowsArray[0]);
+            for (var i = 1; i < imagesArray.length; i++){
+                gallery.appendChild(imagesArray[i]);
+            }
+            gallery.appendChild(imagesArray[0]);
+            gallery.appendChild(thisArrowsArray[1]);
+            console.log(gallery);
+
+            images = gallery.getElementsByClassName("dog_image");
+            imagesArray = Array.from(images);
+            for (let i = 0; i < Math.min(imagesArray.length, numberOfImgs); i++) {
+                imagesArray[i].classList.add("more");
+                imagesArray[i].classList.remove("less");
+                console.log("a1");
+            }
+            if (imagesArray.length > numberOfImgs){
+                for (let i = numberOfImgs; i < imagesArray.length; i++) {
+                    console.log("b1");
+                    imagesArray[i].classList.add("less");
+                    imagesArray[i].classList.remove("more");
+                }
+            }
+        });
+    });
+
+
+    rightArrowsArray.forEach(element => {
+        element.addEventListener("click", function(){
+            let gallery = element.parentElement;
+            let thisArrows = gallery.getElementsByClassName("arrow");
+            let thisArrowsArray = Array.from(thisArrows);
+            let images = gallery.getElementsByClassName("dog_image");
+            var imagesArray = Array.from(images);
+            gallery.innerHTML = '';
+
+            gallery.appendChild(thisArrowsArray[0]);
+            gallery.appendChild(imagesArray[imagesArray.length-1]);
+            for (var i = 0; i < imagesArray.length-1; i++){
+                gallery.appendChild(imagesArray[i]);
+            }
+            gallery.appendChild(thisArrowsArray[1]);
+            console.log(gallery);
+
+            images = gallery.getElementsByClassName("dog_image");
+            imagesArray = Array.from(images);
+            for (let i = 0; i < Math.min(imagesArray.length, numberOfImgs); i++) {
+                console.log("a2");
+                imagesArray[i].classList.add("more");
+                imagesArray[i].classList.remove("less");
+            }
+            if (imagesArray.length > numberOfImgs){
+                for (let i = numberOfImgs; i < imagesArray.length; i++) {
+                    console.log("b2");
+                    imagesArray[i].classList.add("less");
+                    imagesArray[i].classList.remove("more");
+                }
+            }
+        });
+    });
+});
